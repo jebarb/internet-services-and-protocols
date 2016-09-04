00f2eba8daea827176a0a5ee1da7a2127ae497a3
James Barbour <jebarbou@cs.unc.edu>
2016-09-03 23:36:55 -0400

    hw1 started, maybe complete

---
+++ hw1/hw1.py
@@ -0,0 +1,23 @@
+import fileinput
+import re
+
+for line in fileinput.input():
+    print(line.strip())
+    if not re.match("^MAIL.*$", line, flags=re.I):
+        print("ERROR -- MAIL")
+    if not re.match("^( |    ).*$", line[4:len(line)-1], flags=re.I):
+        print("ERROR -- whitespace")
+    elif not re.match("^FROM:.*$", line[4:len(line)-1].strip(), flags=re.I):
+        print("ERROR -- FROM:")
+    elif not re.match("^<.*>$", line[line.index(':')+1:len(line)-1].strip(), flags=re.I):
+        print("ERROR -- reverse-path")
+    elif not re.match("^\S+@\S+$", line[line.index('<')+1:line.index('>')], flags=re.I):
+        print("ERROR -- mailbox")
+    elif not re.match("^[^\s<>()[\]\.,;:@\"]+$", line[line.index('<')+1:line.index('@')], flags=re.I):
+        print("ERROR -- local-part")
+    elif not re.match("^[a-z][a-z0-9]+(\.[a-z][a-z0-9]*)*$", line[line.index('@')+1:line.index('>')], flags=re.I):
+        print("ERROR -- domain")
+    elif not re.match("\n", line[len(line)-1], flags=re.I):
+        print("ERROR -- CRLF")
+    else:
+        print("Sender ok")
