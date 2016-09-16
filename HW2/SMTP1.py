53a4b1e864907723180c68ee0aaad6f0a380b09f
James Barbour <jebarbou@cs.unc.edu>
2016-09-16 17:16:48 -0400

    HW2 cleaned up

--- HW2/SMTP1.py
+++ HW2/SMTP1.py
@@ -4,16 +4,14 @@ import re
 # define regex strings
 nullspace = "( |    )*"
 whitespace = "( |   )+"
-crlf = '\n'
 mail_from = "^MAIL" + whitespace + "FROM:.*$"
 rcpt_to = "^RCPT" + whitespace + "TO:.*$"
 path = "^" + nullspace + "<[^\s<>()[\]\.,;:@\"]+" + \
-        "@[a-z][a-z0-9]+(\.[a-z][a-z0-9]+)*>" + nullspace + crlf + "$"
-data = "^DATA" + nullspace + crlf + "$"
+        "@[a-z][a-z0-9]+(\.[a-z][a-z0-9]+)*>" + nullspace + "\n$"
+data = "^DATA" + nullspace + "\n$"
 end_data = "^<CRLF>.<CRLF>$"
 
 # define states
-state = 0
 start_state = 0     # beginning state, accept MAIL FROM
 rcpt_state = 1      # accept RCPT TO
 rcpt_data_state = 2 # accept DATA or RCPT TO
@@ -22,9 +20,9 @@ invalid_state = -1  # indicates invalid input
 
 # validate command, return state that accepts it
 def command_check(line):
-    if re.match(mail_from, line, flags=re.I):
+    if re.match(mail_from, line, flags=re.I) and argument_check(line):
         return start_state
-    elif re.match(rcpt_to, line, flags=re.I):
+    elif re.match(rcpt_to, line, flags=re.I) and argument_check(line):
         return rcpt_state
     elif re.match(data, line, flags=re.I):
         return data_state
@@ -32,38 +30,36 @@ def command_check(line):
         return invalid_state
 
 # validate command argument
-def argument_check(line, command):
-    if command == data_state:
+def argument_check(line):
+    if re.match(path, line[line.index(':')+1:len(line)], flags=re.I):
+        print("250 OK")
         return True
-    return re.match(path, line[line.index(':')+1:len(line)], flags=re.I)
+    else:
+        print("501 Syntax error in parameters or arguments")
+        return False
+
+#check incorrect state
+def bad_state(command):
+    if command == invalid_state:
+        print("500 Syntax error: command unrecognized")
+    else:
+        print("503 Bad sequence of commands")
+    return start_state
 
 # ensure state aligns with input command, print messages
-def state_check(line):
-    global state
+def state_check(line, state):
     command = command_check(line)
-    if command == data_state and state == rcpt_data_state:
-        print("354 Start mail input; end with <CRLF>.<CRLF>")
-        return command
-    elif command == state:
-        if argument_check(line, command):
-            print("250 OK")
-            return command + 1
-        else:
-            print("501 Syntax error in parameters or arguments")
-            return start_state
-    elif command == rcpt_state and state == rcpt_data_state:
-        if argument_check(line, command):
-            print("250 OK")
-            return state
-        else:
-            print("501 Syntax error in parameters or arguments")
-            return start_state
-    else:
-        if command == invalid_state:
-            print("500 Syntax error: command unrecognized")
-        elif argument_check(line, command):
-            print("503 Bad sequence of commands")
-        return start_state
+    if state == command == start_state:
+        return rcpt_state
+    elif state == command == rcpt_state:
+        return rcpt_data_state
+    elif state == rcpt_data_state:
+        if command == rcpt_state:
+            return rcpt_data_state
+        elif command == data_state:
+            print("354 Start mail input; end with <CRLF>.<CRLF>")
+            return data_state
+    return bad_state(command)
 
 def write_to_file(sender, recipients, email_text):
     for address in recipients:
@@ -79,11 +75,12 @@ def process():
     sender = ""
     recipients = []
     email_text = ""
-    global state
+    state = start_state
     for line in fileinput.input():
         print(line.rstrip())
         if state == data_state:
             if re.match(end_data, line.rstrip(), flags=re.I):
+                print("250 OK")
                 write_to_file(sender, recipients, email_text)
                 state = start_state
                 sender = ""
@@ -92,10 +89,10 @@ def process():
             else:
                 email_text += line
         else:
-            state = state_check(line)
+            state = state_check(line, state)
             if state == rcpt_state:
                 sender = line[line.index(':')+1:len(line)].strip()
-            elif state == rcpt_state or state == rcpt_data_state:
+            elif state == rcpt_data_state:
                 recipients.append(line[line.index(':')+1:len(line)].strip())
 
 process()
