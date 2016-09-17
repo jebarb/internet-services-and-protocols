b9c19d40509aea7ae47b7011f1e72262f5d9d612
James Barbour <jebarbou@cs.unc.edu>
2016-09-17 12:45:13 -0400

    Hw2 cleanup

--- HW2/SMTP1.py
+++ HW2/SMTP1.py
@@ -4,7 +4,7 @@ import re
 # define regex strings
 nullspace = "( |    )*"
 whitespace = "( |   )+"
-mail_from = "^MAIL" + whitespace + "FROM:.*$"
+mail_from = "^MAIL( |   )+FROM:.*$"
 rcpt_to = "^RCPT" + whitespace + "TO:.*$"
 path = "^" + nullspace + "<[^\s<>()[\]\.,;:@\"]+" + \
         "@[a-z][a-z0-9]+(\.[a-z][a-z0-9]+)*>" + nullspace + "\n$"
@@ -12,54 +12,61 @@ data = "^DATA" + nullspace + "\n$"
 end_data = "^<CRLF>.<CRLF>$"
 
 # define states
-start_state = 0     # beginning state, accept MAIL FROM
-rcpt_state = 1      # accept RCPT TO
-rcpt_data_state = 2 # accept DATA or RCPT TO
-data_state = 3      # accept data string or termination sequence
-invalid_state = -1  # indicates invalid input
+start_state = 0      # beginning state, accept MAIL FROM
+rcpt_state = 1       # accept RCPT TO
+rcpt_data_state = 2  # accept DATA or RCPT TO
+data_state = 3       # accept data string or termination sequence
+invalid_state = -1   # indicates invalid input
+bad_arg_state = -2   # indicates invalid argument
 
-# validate command, return state that accepts it
-def command_check(line):
-    if re.match(mail_from, line, flags=re.I) and argument_check(line):
-        return start_state
-    elif re.match(rcpt_to, line, flags=re.I) and argument_check(line):
-        return rcpt_state
+
+def command_check(line):  # validate command, return state that accepts it
+    if re.match(mail_from, line, flags=re.I):
+        return start_state if argument_check(line) else bad_arg_state
+    elif re.match(rcpt_to, line, flags=re.I):
+        return rcpt_state if argument_check(line) else bad_arg_state
     elif re.match(data, line, flags=re.I):
         return data_state
     else:
         return invalid_state
 
+
 # validate command argument
 def argument_check(line):
     if re.match(path, line[line.index(':')+1:len(line)], flags=re.I):
-        print("250 OK")
         return True
     else:
-        print("501 Syntax error in parameters or arguments")
         return False
 
-#check incorrect state
+
+# check incorrect state
 def bad_state(command):
     if command == invalid_state:
         print("500 Syntax error: command unrecognized")
+    elif command == bad_arg_state:
+        print("501 Syntax error in parameters or arguments")
     else:
         print("503 Bad sequence of commands")
     return start_state
 
-# ensure state aligns with input command, print messages
+
+# ensure state aligns with input command
 def state_check(line, state):
     command = command_check(line)
     if state == command == start_state:
+        print("250 OK")
         return rcpt_state
     elif state == command == rcpt_state:
+        print("250 OK")
         return rcpt_data_state
-    elif state == rcpt_data_state:
-        if command == rcpt_state:
-            return rcpt_data_state
-        elif command == data_state:
-            print("354 Start mail input; end with <CRLF>.<CRLF>")
-            return data_state
+    elif state == rcpt_data_state and command == rcpt_state:
+        print("250 OK")
+        return rcpt_data_state
+    elif state == rcpt_data_state and command == data_state:
+        print("354 Start mail input; end with <CRLF>.<CRLF>")
+        return data_state
     return bad_state(command)
+
 
 def write_to_file(sender, recipients, email_text):
     for address in recipients:
@@ -69,6 +76,7 @@ def write_to_file(sender, recipients, email_text):
         for r in recipients:
             out.write("To: " + r + '\n')
         out.write(email_text)
+
 
 def process():
     # store sender, recipients, email text
