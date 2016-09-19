58818e0ad760eba8ff714543ab2cf0e74ce361ff
James Barbour <james@jebarb.com>
2016-09-18 22:07:37 -0400

    HW2 complete

--- HW2/SMTP1.py
+++ HW2/SMTP1.py
@@ -1,86 +1,89 @@
 import fileinput
 import re
 
-# define regex strings
-mail_from = "^MAIL( |   )+FROM:.*$"
-rcpt_to = "^RCPT( |    )+TO:.*$"
-path = "^( |    )*<[^\s<>()[\]\.,;:@\"]+"\
-        "@[a-z][a-z0-9]+(\.[a-z][a-z0-9]+)*>( |    )*\n$"
-data = "^DATA( |    )*\n$"
-end_data = "^<CRLF>.<CRLF>$"
 
-# define states
-state_start = 0      # initial state, accept MAIL FROM
-state_rcpt = 1       # accept RCPT TO
-state_data = 2       # accept RCPT TO or DATA
-state_body = 3       # accept message body or termination sequence
-state_finish = 4     # indicates completed SMTP request
-state_bad_cmd = -1   # indicates invalid command
-state_bad_arg = -2   # indicates invalid argument
+class smtp:  # define regex strings
+    mail_from = "^MAIL( |\t)+FROM:.*$"
+    rcpt_to = "^RCPT(\s|\t)+TO:.*$"
+    path = "^( |\t)*<[^\s<>()[\]\.,;:@\"]+"\
+            "@[a-z][a-z0-9]+(\.[a-z][a-z0-9]+)*>( |\t)*\n$"
+    data = "^DATA( |\t)*\n$"
+    end_data = "^\.\n$"
+
+
+class states:      # define states
+    start = 0      # initial state, accept MAIL FROM
+    rcpt = 1       # accept RCPT TO
+    data = 2       # accept RCPT TO or DATA
+    body = 3       # accept message body or termination sequence
+    finish = 4     # indicates completed SMTP request
+    bad_cmd = -1   # indicates invalid command
+    bad_arg = -2   # indicates invalid argument
 
 
 def command_check(line):  # validate command, return state that accepts it
-    if re.match(mail_from, line):
-        return state_start if argument_check(line) else state_bad_arg
-    elif re.match(rcpt_to, line):
-        return state_rcpt if argument_check(line) else state_bad_arg
-    elif re.match(data, line):
-        return state_data
-    elif re.match(end_data, line.rstrip()):
-        return state_body
-    else:
-        return state_bad_cmd
+    if re.match(smtp.mail_from, line):
+        return states.start if argument_check(line) else states.bad_arg
+    elif re.match(smtp.rcpt_to, line):
+        return states.rcpt if argument_check(line) else states.bad_arg
+    elif re.match(smtp.data, line):
+        return states.data
+    elif re.match(smtp.end_data, line):
+        return states.body
+    return states.bad_cmd
 
 
 def argument_check(line):  # validate command argument
-    return re.match(path, line[line.index(':')+1:])
+    return re.match(smtp.path, line[line.index(':')+1:])
 
 
 def state_check(state, command):  # ensure state+input align, print message
-    if state == command == state_data:
+    if state == command == states.data:
         print("354 Start mail input; end with <CRLF>.<CRLF>")
-        return state_body
-    elif state == command or state == state_data and command == state_rcpt:
+        return states.body
+    elif state == command or (state == states.data and command == states.rcpt):
         print("250 OK")
         return command + 1
-    elif state == state_body:
-        return state_body
-    elif command == state_bad_cmd or command == state_body:
+    elif state == states.body:
+        return states.body
+    elif command in [states.bad_cmd, states.body]:
         print("500 Syntax error: command unrecognized")
-    elif command == state_bad_arg: 
+    elif command == states.bad_arg:
         print("501 Syntax error in parameters or arguments")
     else:
         print("503 Bad sequence of commands")
-    return state_start  # on error, reset to start state
+    return states.start  # on error, reset to start state
 
 
 def write_to_file(sender, recipients, email_text):  # write email to file
     for address in recipients:
-        out = open("forward/" + address[1:-1].strip(), "a+")
+        out = open("forward/" + address[1:-1].strip(), "a+")  # remove <>
         out.write("From: " + sender + '\n')
         for r in recipients:
             out.write("To: " + r + '\n')
         out.write(email_text)
 
 
-def process(): # process input and output
+def process_smtp(): # process input and output
     sender = email_text = ""
     recipients = []
-    state = state_start
+    state = states.start
     for line in fileinput.input():
         print(line.rstrip('\n'))
-        if state == state_start:
+        if state == states.start:
             sender = email_text = ""
             recipients = []
         state = state_check(state, command_check(line))
-        if state == state_rcpt:
+        if state == states.rcpt:
             sender = line[line.index(':')+1:].strip()
-        elif state == state_data:
+        elif state == states.data:
             recipients.append(line[line.index(':')+1:].strip())
-        elif state == state_body:
+        elif state == states.body:
             email_text += line
-        elif state == state_finish:
+        elif state == states.finish:
+            email_text = email_text[email_text.index('\n')+1:]  # remove DATA
             write_to_file(sender, recipients, email_text)
-            state = state_start
+            state = states.start
 
-process()
+
+process_smtp()
