5882e198a129e46611093633ae5d63bb5cbbe02a
James Barbour <jebarbou@cs.unc.edu>
2016-10-02 15:28:28 -0400

    HW3 cleaned up, finished, maybe fix last edge case of emty message body

--- HW3/SMTP2.py
+++ HW3/SMTP2.py
@@ -4,12 +4,10 @@ import sys
 
 
 class smtp:  # define regex
-    path = re.compile("")
     mail_from = re.compile("^From: <[^\s<>()[\]\.,;:@\"]+" +
                            "@[a-z][a-z0-9]+(\.[a-z][a-z0-9]+)*>\n$")
     rcpt_to = re.compile("^To: <[^\s<>()[\]\.,;:@\"]+" +
                          "@[a-z][a-z0-9]+(\.[a-z][a-z0-9]+)*>\n$")
-    finish = re.compile("^.\n$")
 
 
 class states:      # define states
@@ -29,36 +27,42 @@ def command_check(line):  # validate command, return state that accepts it
 
 
 def state_check(line, command, state):  # ensure state sligns with command
-    if state == states.message and command == states.data:
+    if state is states.message and command is states.data:
         print(line.rstrip('\n'))
         return state
-    else:
-        return response_check(line, send_command(line, command, state))
+    return response_check(line, send_command(line, command, state))
 
 
 def send_command(line, command, state):  # write command to stdout
-    if state == states.message:
-        if command == states.start:
+    if state is states.message:
+        if command is states.start:
             print(".")
         return state
-    elif command == states.start:
-        print("MAIL FROM: " + line[line.index('<'):].rstrip('\n'))
-    elif command == states.rcpt:
+    elif command is states.start and state is states.start:
+            print("MAIL FROM: " + line[line.index('<'):].rstrip('\n'))
+    elif command is states.rcpt and state in [states.rcpt, states.data]:
         print("RCPT TO: " + line[line.index('<'):].rstrip('\n'))
-    elif command == states.data:
+    elif command is states.data and state is states.data:
         print("DATA")
+    else:
+        return states.error
     return command
 
 
-def response_check(line, command):
-    response = raw_input()
-    print >> sys.stderr, response
+def response_check(line, command):  # check response against state
+    if command is states.error:
+        return command
+    global input
+    try:
+        input = raw_input  # python 2 and 3 compatibility
+    except NameError:
+        pass
+    response = input()
+    sys.stderr.write(response.rstrip('\n') + "\n")  # ensure one newline char
     if command == states.data:
         if response.startswith("354"):
             print(line.rstrip('\n'))
             return states.message
-        else:
-            return states.error
     elif response.startswith("250"):
         return states.start if command == states.message else command + 1
     return states.error
@@ -69,11 +73,11 @@ def process_email():  # process input and output
     line = ""
     for line in fileinput.input():
         state = state_check(line, command_check(line), state)
-        if state == states.start:
+        if state is states.start:
             state = state_check(line, command_check(line), state)
-        if state == states.error:
+        if state is states.error:
             break
-    if state == states.message:
+    if state is states.message:
         response_check(line, send_command(line, states.start, state))
     print("QUIT")
 
